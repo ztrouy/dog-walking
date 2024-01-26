90214fd70c168961e350fb119b01a9a1c560756e
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-26 15:18:02 -0600

    Added states to the generated HTML for RegisteredPets

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -6,7 +6,10 @@ export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li data-id="${pet.id}" 
+                        data-walkerId="${pet.walkerId}"
+                        >${pet.name}
+                    </li>`
     }
 
     petHTML += "</ul>"
