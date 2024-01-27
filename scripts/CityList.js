77862a15228443a04f2705a1b4c315c7ca6c572c
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-27 15:14:06 -0600

    Added metadata to HTML from CityList.js

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -7,7 +7,10 @@ export const CityList = () => {
     let citiesHTML = "<ul>"
 
     for (const walker of walkers) {
-        citiesHTML += `<li>${walker.city}</li>`
+        citiesHTML += `<li data-walkerName="${walker.name}"
+                            data-type="city"
+                            >${walker.city}
+                        </li>`
     }
 
     citiesHTML += "</ul>"
