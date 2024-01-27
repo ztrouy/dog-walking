0babecb9e092500d2bd0e74349e95c31a4e1d6f3
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-27 14:33:36 -0600

    Added basic event listener to walkers.js

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -3,6 +3,14 @@ import { getWalkers } from "./database.js"
 const walkers = getWalkers()
 
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const elementClickedOn = clickEvent.target
+
+        window.alert(`This walker works in ${elementClickedOn.dataset.city}`)
+    })
+
 export const Walkers = () => {
     let walkerHTML = "<ul>"
 
