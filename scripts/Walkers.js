bc713e768997e529c8f98399420e1bf5dbb57f4b
Zachary Trouy <ztrouy@gmail.com>
2024-02-08 10:45:34 -0600

    Moved getWalkers from module scope to function scope

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,19 +1,19 @@
 import { getWalkers } from "./database.js"
 
-const walkers = getWalkers()
-
 
 document.addEventListener(
     "click",
     (clickEvent) => {
         const elementClickedOn = clickEvent.target
-
+        
         if (elementClickedOn.dataset.type === "walker") {
             window.alert(`This walker works in ${elementClickedOn.dataset.city}`)
         }
     })
-
-export const Walkers = () => {
+    
+    export const Walkers = () => {
+    const walkers = getWalkers()
+    
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
